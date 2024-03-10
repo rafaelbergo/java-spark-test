512d89db742698aa2b3c400100f0cef22cb24b83
Rafael Bergo <rafaelbergo503@gmail.com>
2024-03-10 01:50:33 -0300

    Update Maven and add Spark SQL dependency

--- src/main/java/com/sparktest/App.java
+++ src/main/java/com/sparktest/App.java
@@ -1,13 +1,24 @@
 package com.sparktest;
 
-/**
- * Hello world!
- *
- */
+//import java.util.logging.Logger;
+
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws Exception {
+        SparkSession spark = SparkSession.builder().appName("Spark Test").master("local[*]").getOrCreate();
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+        
+        Dataset<Row> emp = spark.read().option("header", true).csv("path");
+        emp.show();
+        spark.close();
+
     }
 }
