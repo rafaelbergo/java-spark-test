a87fd19d14037e4d214c009867b01be62d843eca
Rafael Bergo <rafaelbergo503@gmail.com>
2024-03-18 13:55:55 -0300

    removido codigo comentado

--- src/main/java/com/LinearRegression/LinearRegression.java
+++ src/main/java/com/LinearRegression/LinearRegression.java
@@ -18,17 +18,12 @@ public class LinearRegression {
         //dados.printSchema();
         //dados.summary().show();
  
-
         VectorAssembler assembler = new VectorAssembler()
             .setInputCols(new String[] {"bedrooms", "bathrooms", "sqft_living", "sqft_lot", "sqft_above", "sqft_basement"})
             .setOutputCol("features")
             .setHandleInvalid("skip"); // pula a linha que tiver algum valor nulo
         
         Dataset<Row> dadosFeatures = assembler.transform(dados).select("price", "features").withColumnRenamed("price", "label");
-        //dadosFeatures.show();
-
-        //LinearRegression lr = new LinearRegression();
-        //LinearRegressionModel lrModel = lr.fit(dadosFeatures);
 
         // Create a LinearRegression object
         org.apache.spark.ml.regression.LinearRegression lr = new org.apache.spark.ml.regression.LinearRegression();
